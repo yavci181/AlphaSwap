13a67176476852205331849ad1e28597ac4c080d
yavci181 <42972791+yavci181@users.noreply.github.com>
2021-08-17 17:35:55 +0000

    Update index.ts

--- pancake-swap-interface-v1/src/connectors/index.ts
+++ pancake-swap-interface-v1/src/connectors/index.ts
@@ -8,7 +8,7 @@ import { NetworkConnector } from './NetworkConnector'
 
 const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
 
-export const NETWORK_CHAIN_ID: number = parseInt(process.env.REACT_APP_CHAIN_ID ?? '97')
+export const NETWORK_CHAIN_ID: number = parseInt(process.env.REACT_APP_CHAIN_ID ?? '56')
 
 if (typeof NETWORK_URL === 'undefined') {
   throw new Error(`REACT_APP_NETWORK_URL must be a defined environment variable`)
@@ -28,7 +28,7 @@ export const injected = new InjectedConnector({
   supportedChainIds: [97],
 })
 
-export const bscConnector = new BscConnector({ supportedChainIds: [97] })
+export const bscConnector = new BscConnector({ supportedChainIds: [56] })
 
 // mainnet only
 export const walletconnect = new WalletConnectConnector({
